a0b3f3804c894325318c9342a0be1910cde0d4b5
Sanjana2707 <sanjanagowda2707@gmail.com>
2025-09-19 12:15:18 +0530

    feat: dispatched increment action

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -8,10 +8,11 @@ import { Store } from '@ngrx/store';
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css'],
 })
-export class CounterOutputComponent implements OnInit, OnDestroy {
+export class CounterOutputComponent {
   count$: Observable<number>;  // $ sign is a convention to indicate its an observable
 
-  constructor(private store:Store<{counter:number}>) {   // inject store , we can use since its provided in root and the type of store is defined here because we have to tell what kind of data we are expecting from the store orlese typescript wont know what kind of data we are expecting from the store
+  constructor(private store:Store<{counter:number}>) {   // inject store , we can use since its provided in rootand the type of store is defined here
+  //  because we have to tell what kind of data we are expecting from the store orlese typescript wont know what kind of data we are expecting from the store
          this.count$=store.select('counter'); //counter is the key we defined in app.module.ts which is used to map the reducer and select is used to get the data from the store
 
   
